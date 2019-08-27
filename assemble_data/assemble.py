46403c37d2cde9228056cc79d18f0b2b25358548
Pierre-Yves Taunay <5438710+pytaunay@users.noreply.github.com>
2019-08-27 15:43:20 -0400

    Finished script to assemble data from all the pressure plots into a single dataframe

--- assemble_data/assemble.py
+++ assemble_data/assemble.py
@@ -75,6 +75,8 @@ for idx in pdf.index:
     Tovec = pdf.To[idx]
     dcvec = pdf.dc[idx]
     
+    Twvec[Twvec == 1000.] = np.nan
+    
     length = len(Idvec)
 
     # Type of cathode?
@@ -150,7 +152,167 @@ for idx in pdf.index:
         alldata = alldata.append(tmp,ignore_index=True)
 
 ### Populate references
-alldata.loc[0:4,'reference'] = 'K. K. Jameson, D. M. Goebel, and R. M. Watkins, "Hollow Cathode and Keeper-Region Plasma Measurements," 41st AIAA/ASME/SAE/ASEE Joint Propulsion Conference and Exhibit, 2005.'
-alldata.loc[0:4,'note'] = 'Figure 3 - Pressure measured inside the 1/4" hollow cathode for various NSTAR throttle levels. The throttle levels TH4, TH8, TH15 are from: V. Rawlin, J. Sovey, J. Anderson, and J. Polk, "NSTAR flight thruster qualification testing," 34th AIAA/ASME/SAE/ASEE JPC, 1998.TH12: W. G. Tighe, K. Chien, D. M. Goebel, and R. T. Longo, “Hollow Cathode Ignition and Life Model,” 41st AIAA/ASME/SAE/ASEE Jt. Propuls. Conf. Exhib., 2005.
+# NSTAR
+NSTAR_ref = [("K. K. Jameson, D. M. Goebel, and R. M. Watkins,"
+             "Hollow Cathode and Keeper-Region Plasma Measurements," 
+             "41st JPC, 2005."),
+             ("I. G. Mikellides, \"Effects of viscosity in a partially ionized " 
+             "channel flow with thermionic emission,\" Phys. Plasmas, vol. 16, " 
+             "no. 1, 2009."),
+             ("J. Polk, A. Grubisic, N. Taheri, D. Goebel, R. Downey, and " 
+             "S. E. Hornbeck, \"Emitter Temperature Distributions in the NSTAR " 
+             "Discharge Hollow Cathode,\" 41st JPC, 2005.")]   
+NSTAR_notes = [("Fig 3 - Pressure measured inside the 1/4\" hollow cathode " 
+               "for various NSTAR throttle levels. The throttle levels TH4, " 
+               "TH8, TH15 are from: V. Rawlin, J. Sovey, J. Anderson, and " 
+               "J. Polk, \"NSTAR flight thruster qualification testing, " 
+               "34th AIAA/ASME/SAE/ASEE JPC, 1998. "
+               "TH12: W. G. Tighe, K. Chien, D. M. Goebel, and R. T. Longo, " 
+               "\"Hollow Cathode Ignition and Life Model,\" 41st JPC, 2005. "
+               "Tw is calculated with Polk's fit"),
+               ("Fig. 10 - Comparisons between measured and computed " 
+               "values of the cathode gas pressure. "
+               "Tw is calculated with Polk's fit"),
+               ("Fig.4 - Comparison of temperatures from the NSTAR cathode " 
+               "and the plasma contactor. "
+               "Fig.8 - Internal hollow cathode pressure increases with flow"
+               "rate and discharge current")]
+          
+alldata.loc[0:3,'reference'] = NSTAR_ref[0]
+alldata.loc[0:3,'note'] = NSTAR_notes[0]
+alldata.loc[4:24,'reference'] = NSTAR_ref[1]
+alldata.loc[4:24,'note'] = NSTAR_notes[1]
+alldata.loc[25:42,'reference'] = NSTAR_ref[2]
+alldata.loc[25:42,'note'] = NSTAR_notes[2]
+
+# NEXIS
+NEXIS_ref = [("K. K. Jameson, D. M. Goebel, and R. M. Watkins, \"Hollow Cathode " 
+"and Thruster Discharge Chamber Plasma Measurements Using "
+"High-Speed Scanning Probes,\" 29th International Electric Propulsion Conference, 2005."),
+("D. Goebel, K. K. Jameson, R. M. Watkins, and I. Katz, "
+"\"Hollow Cathode and Keeper-Region Plasma Measurements Using Ultra-Fast Miniature Scanning Probes,\" "
+"40th JPC, 2004.")]
+NEXIS_notes = [("Fig. 7 - Internal NEXIS cathode pressure (a) constant flow "
+               "rate at 5.5 sccm (b) constant discharge current at 22 A."),
+               ("Fig. 8 - Pressure measured and calculated inside the hollow "
+               "cathode without plasma (a) and with plasma (b)")]
+
+alldata.loc[43:56,'reference'] = NEXIS_ref[0]
+alldata.loc[43:56,'note'] = NEXIS_notes[0]
+alldata.loc[57:63,'reference'] = NEXIS_ref[1]
+alldata.loc[57:63,'note'] = NEXIS_notes[1]
+
+# Salhi
+Salhi_ref = ("A. Salhi, \"Theoretical and experimental studies of orificed, " 
+"hollow cathode operation,\" The Ohio State University, 1993")
+Salhi_notes = [("Xe, mdot = 0.5 A, T vs Id for 1.21 mm: Fig 5.68"),
+               ("Xe, mdot = 0.93 A, T vs Id for 1.21 mm: Fig 6.3"),
+               ("Ar, mdot = 0.5 A, T vs Id for 1.21 mm: Fig 5.68. Measurements are taken 6 mm away from the orifice"),
+               ("Ar, mdot = 0.93 A, T vs Id for 1.27 mm: Fig 5.70. We consider "
+               "1.27 mm approximately equal to 1.21 mm to keep same orifice sizes "
+               "(possibly typo)"),
+               ("Ar, mdot = 0.93 A, T vs Id for 0.76 mm: Fig 5.70 ")
+               ]
+
+alldata.loc[alldata.cathode == 'Salhi-Xe','reference'] = Salhi_ref
+alldata.loc[alldata.cathode == 'Salhi-Ar-1.21','reference'] = Salhi_ref
+alldata.loc[alldata.cathode == 'Salhi-Ar-0.76','reference'] = Salhi_ref
+
+# Xenon, mass flow of 0.5 A and 0.93 A
+boolcond = (alldata.cathode == 'Salhi-Xe') & (alldata.massFlowRate == 0.5)
+alldata.loc[boolcond,'note'] = Salhi_notes[0]
+boolcond = (alldata.cathode == 'Salhi-Xe') & (alldata.massFlowRate == 0.93)
+alldata.loc[boolcond,'note'] = Salhi_notes[1]
+
+# Argon
+boolcond = (alldata.cathode == 'Salhi-Ar-1.21') & (alldata.massFlowRate == 0.5)
+alldata.loc[boolcond,'note'] = Salhi_notes[2]
+boolcond = (alldata.cathode == 'Salhi-Ar-1.21') & (alldata.massFlowRate == 0.93)
+alldata.loc[boolcond,'note'] = Salhi_notes[3]
+boolcond = (alldata.cathode == 'Salhi-Ar-0.76') & (alldata.massFlowRate == 0.93)
+alldata.loc[boolcond,'note'] = Salhi_notes[4]
+
+
+# Siegfried
+siegfried_refs = ("[1] P. J. Wilbur, \"Ion and advanced electric thruster research,\" CR-165253, 1980., "
+"[2] Siegfried, D. E., Wilbur, P. J. \"Phenomenological model describing orificed, hollow cathode operation\", 15th IEPC, 1981 "
+"[3] Siegfried, D. E. \"A Phenomenological Model for Orificed Hollow Cathodes\", Ph.D. thesis, Colorado State University, 1982")
+siegfried_notes = ("# Mercury cathode with a 0.76mm diameter orifice "
+"Table p. 14 of Ref. [1], p.4 of Ref. [2]. "
+"Temperature data for the cases at 3.3 A are from Ref. [2] "
+"Orifice length is from Ref. [3], p.130 "
+"The insert temperature for the experiments at fixed Id = 3.31 A but "
+"varying mass flow rate are taken to be the same as the one reported for 3.31 A")
+
+alldata.loc[alldata.cathode=='Siegfried','reference'] = siegfried_refs
+alldata.loc[alldata.cathode=='Siegfried','note'] = siegfried_notes
+
+# Domonkos
+domonkos_ref = ("M. T. Domonkos, \"Evaluation of low-current orificed hollow "
+"cathodes,\" University of Michigan, 1999.")
+
+# AR3
+ar3_note = ("Reduction of Cathode Internal Pressure for Spot-Mode Operation "
+"Fig 3.27 Orifice plate temperature dependence on aspect ratio in a 1.00-A discharge to the keeper "
+"Extracted from the overall plot; this data is for the AR3, at Id = 1A. "
+"The wall temperature is here equal to the orifice plate temperature ")
+alldata.loc[alldata.cathode=='AR3','reference'] = domonkos_ref
+alldata.loc[alldata.cathode=='AR3','note'] = ar3_note
+alldata.loc[alldata.cathode=='AR3','orificeTemperature'] = alldata.loc[alldata.cathode=='AR3','insertTemperatureAverage']
+
+# EK6
+ek6_note = ("Cathode Internal Pressure Under Various Operating Conditions "
+"Fig 3.29: Orifice plate temperature dependence on keeper type in a diode discharge "
+"Extracted from the overall plot; this data is for the EK6. "
+"The temperature data is the orifice plate temperature")
+alldata.loc[alldata.cathode=='EK6','reference'] = domonkos_ref
+alldata.loc[alldata.cathode=='EK6','note'] = ek6_note
+alldata.loc[alldata.cathode=='EK6','orificeTemperature'] = alldata.loc[alldata.cathode=='EK6','insertTemperatureAverage']
+
+# SC012
+sc012_note = ("Reduction of Cathode Internal Pressure for Spot-Mode Operation "
+"Extracted from the overall plot; this data is for the SC012. "
+"The mass flow rate values are not exactly the same from one run to the other "
+"one, so I kept them as raw data values extracted from the plot")
+alldata.loc[alldata.cathode=='SC012','reference'] = domonkos_ref
+alldata.loc[alldata.cathode=='SC012','note'] = ek6_note
+
+
+
+# Friedly
+friedly_ref = ("V. J. Friedly, \"Hollow cathode operation at high discharge currents\", "
+"M. Sc. Thesis, NASA CR-185238, 1990")
+friedly_note = ("Fig 30 p.65: Effects of discharge current and cathode "
+"configuration on cathode wall temperature "
+"Fig 33a p.69: Effects of discharge current and propellant flowrate on cathode "
+"internal pressure; "
+"Xenon cathode; "
+"6.4 mm outer tube, 0.74 mm orifice diameter => 4.7 mm insert diameter; "
+"Lo is not specified so we set it as 1 mm; "
+"The cathode wall temperature is plotted with a single point for a flow rate; " 
+"range of 180 to 550 mA; we consider that the wall temperature stays constant "
+"with flow rate (typically an acceptable assumption).")
+
+alldata.loc[alldata.cathode=='Friedly','reference'] = friedly_ref
+alldata.loc[alldata.cathode=='Friedly','note'] = friedly_note
+
+# T6
+t6_ref = ("[1] S. W. Patterson, D. G. Fearn, \"The generation of high energy "
+          "ions in hollow cathode discharges\", IEPC, 1999; "
+          "[2] D. G. Fearn, S. W. Patterson, \"Characterisation of the high "
+          "current hollow cathode for the T6 thruster\", 34th JPC, 1998")
+t6_notes = ("[1] Fig 15 Measured pressure as mdot is varied; "
+            "[2] Fig 16 The influence of orifice diameter and flow rate on " 
+            "temperature at 15 A; "
+            "Extracted from Fig. 15. Gas is xenon. Mass flow rate error: 1 to " 
+            "2%. The orifice diameter is probably 1 mm as Ref. [1] refers to "
+            "Ref. [2], which uses 1 mm in Figs 6 and 12. [2] Fig 16 The wall "
+            "temperature is the exterior casing temperature, NOT the insert temperature.")
+
+
+
+alldata.loc[alldata.cathode=='T6','reference'] = t6_ref
+alldata.loc[alldata.cathode=='T6','note'] = t6_notes
+
 
 
