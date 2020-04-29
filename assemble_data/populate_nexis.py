786caad085ac1241bd4de3b055626bc3e65bd6fd
Pierre-Yves Taunay <5438710+pytaunay@users.noreply.github.com>
2020-04-29 17:49:49 -0400

    Add average electron temperature caclulation

--- assemble_data/populate_nexis.py
+++ assemble_data/populate_nexis.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cathode.constants as cc
 
-from compute_attachment_length import compute_attachment_length
+from compute_attachment_length import compute_attachment_length, compute_average_temperature
 
 
 def populate_mikellides_jap_2005(alldata,root,cat_root):
@@ -25,12 +25,14 @@ def populate_mikellides_jap_2005(alldata,root,cat_root):
     idxmax = 10
     Lem_xp, Lem_err = compute_attachment_length(ne_data, dc, idxmin=-idxmin, idxmax=-idxmax)
     
+    
     # Then get temperature and potential
     data = np.genfromtxt(root + cat_root + 'Te-phip_vs_x_mdot-5.5sccm_Id-25A.csv',
                          skip_header = True,
                          delimiter=',')
     phip_data = data[~np.isnan(data[:,1])][:,0:2]
     Te_data = data[~np.isnan(data[:,2])][:,0::2]
+    Te_xp, Te_err = compute_average_temperature(Te_data, dc)
     
     # We can arguably get the average pressure from the two closest cases
     # that are at 24 A and 26 A, 5.5 sccm
@@ -55,7 +57,9 @@ def populate_mikellides_jap_2005(alldata,root,cat_root):
                               'reference': ref,
                               'note': 'Fig. 5',
                               'attachmentLength': Lem_xp,
-                              'attachmentLength_err': Lem_err
+                              'attachmentLength_err': Lem_err,
+                              'electronTemperatureAverage': Te_xp,
+                              'electronTemperatureAverage_err': Te_err
                               } , ignore_index=True)
     
     return alldata
@@ -75,6 +79,7 @@ def populate_goebel_jpc_2004(alldata,root,cat_root):
     
     Te_data = data[~np.isnan(data[:,1])][:,[0,1]]
     phip_data = data[~np.isnan(data[:,2])][:,[0,2]]
+    
     
     bcond = (alldata.cathode=='NEXIS') & (alldata.dischargeCurrent == 10.) & (alldata.massFlowRate == 5.5*cc.sccm2eqA)
     alldata.loc[bcond,'electronTemperature'] = alldata.loc[bcond,'electronTemperature'].apply(lambda x: np.copy(Te_data))
@@ -105,6 +110,8 @@ def populate_goebel_jpc_2004(alldata,root,cat_root):
     idxmin = 40 # From the data
     idxmax = 10
     Lem_xp, Lem_err = compute_attachment_length(ne_data, dc, idxmin=-idxmin, idxmax=-idxmax)
+    Te_xp, Te_err = compute_average_temperature(Te_data, dc)
+    
     
     data = np.genfromtxt(root + cat_root + 'Te-phip_vs_x_mdot-5.5-10sccm_Id-10-25A.csv',
                          skip_header = True,
@@ -129,7 +136,9 @@ def populate_goebel_jpc_2004(alldata,root,cat_root):
                               'reference': ref,
                               'note': 'Fig. 12',
                               'attachmentLength': Lem_xp,
-                              'attachmentLength_err': Lem_err                              
+                              'attachmentLength_err': Lem_err,
+                              'electronTemperatureAverage': Te_xp,
+                              'electronTemperatureAverage_err': Te_err                              
                               } , ignore_index=True)  
     
     return alldata
--- assemble_data/populate_nstar.py
+++ assemble_data/populate_nstar.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cathode.constants as cc
 
-from compute_attachment_length import compute_attachment_length
+from compute_attachment_length import compute_attachment_length, compute_average_temperature
 
 def populate_th8(alldata,root,cat_root):
     ### TH8
@@ -24,6 +24,8 @@ def populate_th8(alldata,root,cat_root):
     dc = np.unique(alldata[alldata.cathode=='NSTAR'].insertDiameter)[0]
     npoints = 50 # From the data
     Lem_xp, Lem_err = compute_attachment_length(ne_data, dc, idxmin=-npoints)
+    Te_xp, Te_err = compute_average_temperature(Te_data, dc)
+    
     
     bcond = (alldata.cathode=='NSTAR') & (alldata.dischargeCurrent == 8.29) & (alldata.massFlowRate == 2.47*cc.sccm2eqA)
     alldata.loc[bcond,'electronDensity'] = alldata.loc[bcond,'electronDensity'].apply(lambda x: np.copy(ne_data))
@@ -31,6 +33,8 @@ def populate_th8(alldata,root,cat_root):
     alldata.loc[bcond,'plasmaPotential'] = alldata.loc[bcond,'plasmaPotential'].apply(lambda x: np.copy(phip_data))
     alldata.loc[bcond,'attachmentLength'] = Lem_xp
     alldata.loc[bcond,'attachmentLength_err'] = Lem_err
+    alldata.loc[bcond,'electronTemperatureAverage'] = Te_xp
+    alldata.loc[bcond,'electronTemperatureAverage_err'] = Te_err
     
     return alldata
 
@@ -55,6 +59,7 @@ def populate_th15(alldata,root,cat_root):
     dc = np.unique(alldata[alldata.cathode=='NSTAR'].insertDiameter)[0]
     npoints = 50 # From the data
     Lem_xp, Lem_err = compute_attachment_length(ne_data, dc, idxmin=-npoints)
+    Te_xp, Te_err = compute_average_temperature(Te_data, dc)
     
     bcond = (alldata.cathode=='NSTAR') & (alldata.dischargeCurrent == 13.2) & (alldata.massFlowRate == 3.7*cc.sccm2eqA)
     alldata.loc[bcond,'electronDensity'] = alldata.loc[bcond,'electronDensity'].apply(lambda x: np.copy(ne_data))
@@ -62,5 +67,8 @@ def populate_th15(alldata,root,cat_root):
     alldata.loc[bcond,'plasmaPotential'] = alldata.loc[bcond,'plasmaPotential'].apply(lambda x: np.copy(phip_data))
     alldata.loc[bcond,'attachmentLength'] = Lem_xp
     alldata.loc[bcond,'attachmentLength_err'] = Lem_err
+    alldata.loc[bcond,'electronTemperatureAverage'] = Te_xp
+    alldata.loc[bcond,'electronTemperatureAverage_err'] = Te_err
+
     
     return alldata
