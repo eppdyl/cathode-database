ff6a7acb067b15d591d49ba0a01fc8f180f0d4ba
Pierre-Yves Taunay <5438710+pytaunay@users.noreply.github.com>
2020-04-29 15:12:41 -0400

    Compute Pd distribution by first assembling data then using the new field pressureDiameter

--- assemble_data/pd_distribution.py
+++ assemble_data/pd_distribution.py
@@ -3,37 +3,19 @@ Load the pressure data and display total pressure x diameter for all cathodes
 """
 
 import numpy as np
-import pandas as pd
-import pickle
-import matplotlib.pyplot as plt
-import cathode.constants as cc
-
-from cathode.experimental.load_data import load_all_data
-from build_numerical import build_zerod_dataframe
-
 from sklearn.neighbors import KernelDensity
-from sklearn.grid_search import GridSearchCV
+from sklearn.model_selection import GridSearchCV
 #from sklearn.cross_validation import LeaveOneOut
 
-### FIRST CALCULATE THE TOTAL PRESSURE-DIAMETER PRODUCT
-# Load total pressure data
-pdf = load_all_data()
-ldf = pdf['P'] * pdf['dc'] * 0.1
+import matplotlib.pyplot as plt
 
-Pdarr = np.array([])
-for cat in ldf.index:
-    vec = ldf[cat]
-    Pdarr = np.concatenate((Pdarr,vec))
-    
-plhc_data = pickle.load(open("20190404.pkl","rb"))
-for mdot in plhc_data:
-    data = plhc_data[mdot]
-    
-    Pd = data[:,2] * 2.715
-    
-    Pdarr = np.concatenate((Pdarr,Pd))
-    
-    
+from assemble import assemble
+
+data = assemble()
+
+Pdarr = np.array(data.pressureDiameter)
+Pdarr = Pdarr[~np.isnan(Pdarr)]
+
 ## KERNEL DENSITY
 # Calculate best kernel density bandwidth
 bandwidths = 10 ** np.linspace(0, 1, 200)
@@ -56,32 +38,17 @@ x_d = np.linspace(0,100,1000)
 logprob = kde.score_samples(x_d[:,None])
 
 ### CORRECT DOMONKOS
-pdf = load_all_data()
-ldf = pdf['P'] * pdf['dc'] * 0.1
-ldf_do = pdf['P'] * pdf['do'] * 0.1
+for cat in ['SC012','EK6','AR3']:
+    Ptorr = data.loc[data['cathode'] == cat, 'totalPressure']
+    do = data.loc[data['cathode'] == cat, 'orificeDiameter'] * 0.1
+    data.loc[data['cathode'] == cat, 'pressureDiameter'] = Ptorr * do
 
-Pdarr_corr = np.array([])
-for cat in ldf.index:
-    if (cat == 'SC012' or
-        cat == 'EK6' or
-        cat == 'AR3'):
-        vec = ldf_do[cat]
-    else:     
-        vec = ldf[cat]
-    
-    Pdarr_corr = np.concatenate((Pdarr_corr,vec))
-    
-plhc_data = pickle.load(open("20190404.pkl","rb"))
-for mdot in plhc_data:
-    data = plhc_data[mdot]
-    
-    Pd = data[:,2] * 2.715
-    
-    Pdarr_corr = np.concatenate((Pdarr_corr,Pd))
-    
+Pdarr_corr = np.array(data.pressureDiameter)
+Pdarr_corr = Pdarr_corr[~np.isnan(Pdarr_corr)]
+
 ## KERNEL DENSITY
 # Calculate best kernel density bandwidth
-bandwidths = 10 ** np.linspace(0, 1, 200)
+bandwidths = 10 ** np.linspace(-1, 1, 200)
 grid = GridSearchCV(KernelDensity(kernel='gaussian'),
                     {'bandwidth': bandwidths},
                     cv=5,
@@ -100,44 +67,16 @@ kde.fit(Pdarr_corr[:,None])
 x_d = np.linspace(0,100,1000)
 logprob_corr = kde.score_samples(x_d[:,None])
 
-#
-#
-#### NOW CALCULATE THE NEUTRAL GAS PRESSURE-DIAMETER PRODUCT FROM 0D RESULTS
-#zerod = build_zerod_dataframe()
-#ldf = zerod['neutralPressureAverage'] /cc.Torr * zerod['insertDiameter'] * 0.1
-#
-#Pdarr_zd = np.array([ldf])
-#Pdarr_zd = Pdarr_zd[~np.isnan(Pdarr_zd)]
-#
-#    
-### KERNEL DENSITY
-## Calculate best kernel density bandwidth
-#grid = GridSearchCV(KernelDensity(kernel='gaussian'),
-#                    {'bandwidth': bandwidths},
-#                    cv=5,
-#                    verbose = 1)
-#grid.fit(Pdarr_zd[:,None])
-#
-#print('Best params:',grid.best_params_)
-#
-## Instantiate and fit the KDE model
-#print("Instantiate and fit the KDE model")
-#kde = KernelDensity(bandwidth=grid.best_params_['bandwidth'], 
-#                    kernel='gaussian')
-#kde.fit(Pdarr_zd[:,None])
-#
-## Score_samples returns the log of the probability density
-#x_d = np.linspace(0,100,1000)
-#logprob_zd = kde.score_samples(x_d[:,None])
 
 ### Plots
-plt.hist(Pdarr,bins=40,normed=True,histtype='step')
-plt.hist(Pdarr_corr,bins=40,normed=True,histtype='step')
-
-plt.fill_between(x_d, np.exp(logprob), alpha=0.5)
-plt.plot(Pdarr, np.full_like(Pdarr, -0.01), '|k', markeredgewidth=1)
+fig, ax = plt.subplots(2,1)
 
 
-plt.fill_between(x_d, np.exp(logprob_corr), alpha=0.5)
-plt.plot(Pdarr_corr, np.full_like(Pdarr_corr, -0.05), '|k', markeredgewidth=1)
+ax[0].hist(Pdarr,bins=40,normed=True,histtype='step')
+ax[0].fill_between(x_d, np.exp(logprob), alpha=0.5)
+ax[0].plot(Pdarr, np.full_like(Pdarr, -0.01), '|k', markeredgewidth=1)
+
+ax[1].hist(Pdarr_corr,bins=40,normed=True,histtype='step')
+ax[1].fill_between(x_d, np.exp(logprob_corr), alpha=0.5)
+ax[1].plot(Pdarr_corr, np.full_like(Pdarr_corr, -0.05), '|k', markeredgewidth=1)
 
