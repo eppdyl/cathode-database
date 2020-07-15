405a17fe73d2eff3b9324c0964c592ea07eff8af
Pierre-Yves Taunay <5438710+pytaunay@users.noreply.github.com>
2020-07-15 16:08:37 -0400

    Fix multiline strings. Add option for the computation of pi products

--- assemble_data/make_hdf5_database.py
+++ assemble_data/make_hdf5_database.py
@@ -30,28 +30,45 @@ The script to make the HDF5 library.
 import argparse
 import h5py
 import datetime
+import numpy as np
 
 from derived_quantities import generate_dataframe_derived 
 from load_all_data import generate_dataframe
 
 ### Parse arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("-p", "--pressureEmpirical", help="Should the pressure be\
-        calculated with the empirical correlation if not provided?", 
+parser.add_argument("-pe", "--pressureEmpirical", help="Should the pressure "\
+        "be calculated with the empirical correlation if not provided?", 
         action='store_true')
-parser.add_argument("-d", "--derivedQuantity", help="Should derived\
-        quantities be calculated?", action='store_true')
+parser.add_argument("-d", "--derivedQuantity", help="Should derived "\
+        "quantities be calculated?", action='store_true')
+parser.add_argument("-pp","--piProduct", help="Do we calculate the Pi "\
+        " products for the empirical analysis of the total pressure?",
+        action='store_true')
 parser.add_argument("-f", "--filename", help="Filename for the HDF5 file.",
         type=str, default="cathode_database.h5") 
 
 args = parser.parse_args()
 
+if args.pressureEmpirical and args.piProduct:
+    raise ValueError("Cannot have both the pressure calculated with "\
+            "empirical correlation and the Pi products for the empirical "\
+            "analysis of the pressure computed at the same time!")
+
 ### Generate the dataframe
 if args.pressureEmpirical:
-    data = generate_dataframe_derived(empirical_pressure = args.pressureEmpirical)
+    print("INFO Database with both empirical pressure and derived quantities")
+    data = generate_dataframe_derived(empirical_pressure = 
+            args.pressureEmpirical)
+elif args.piProduct:
+    print("INFO Database with both total pressure Pi products and derived\
+            quantities")
+    data = generate_dataframe_derived(pi_products = args.piProduct)
 elif args.derivedQuantity:
+    print("INFO Database with derived quantities ONLY")
     data = generate_dataframe_derived()
 else:
+    print("INFO Database with no derived quantities")
     data = generate_dataframe()
 
 ### Dump to HDF5
