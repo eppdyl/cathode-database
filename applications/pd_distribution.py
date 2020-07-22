cd3cb735fcf401ae76d74a7c42779a506aa11757
Pierre-Yves Taunay <5438710+pytaunay@users.noreply.github.com>
2020-07-22 15:36:30 -0400

    Add license and update location of database

--- applications/pd_distribution.py
+++ applications/pd_distribution.py
@@ -1,17 +1,43 @@
-""" pd_distribution.py
+# MIT License
+# 
+# Copyright (c) 2019-2020 Pierre-Yves Taunay 
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+""" 
+File: pd_distribution.py
+Author: Pierre-Yves Taunay
+Date: 2019
+
 Load the pressure data and display total pressure x diameter for all cathodes
 """
 
 import numpy as np
 from sklearn.neighbors import KernelDensity
 from sklearn.model_selection import GridSearchCV
-#from sklearn.cross_validation import LeaveOneOut
 
 import matplotlib.pyplot as plt
+import pandas as pd
 
-from assemble import assemble
 
-data = assemble()
+data = pd.read_hdf("cathode_database.h5",key="data")
+
 
 Pdarr = np.array(data[['pressureDiameter']].dropna())
 
